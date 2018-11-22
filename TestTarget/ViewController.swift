075caa4df3634e15d3ee757076a0aa226a7f392f
Milos Dimic <milos.dimic@htecgroup.com>
2018-11-22 07:13:08 +0100

    Initial Commit

---
+++ TestTarget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTarget
+//
+//  Created by Milos Dimic on 11/22/18.
+//  Copyright © 2018 Milos Dimic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
